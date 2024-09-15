69b60be268bef5da974835a4b371d3febcde2e49
Jahnavi Malhotra <jahnavim2105@gmail.com>
2024-09-15 03:42:14 -0400

    added components to result page

--- src/views/custom-components/result.jsx
+++ src/views/custom-components/result.jsx
@@ -1,39 +1,93 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { Row, Col, Container, Card, CardBody } from 'reactstrap';
+
+import img1 from '../../assets/images/portfolio/img1.jpg';
+import img2 from '../../assets/images/portfolio/img2.jpg';
+import img3 from '../../assets/images/portfolio/img3.jpg';
+import img4 from '../../assets/images/portfolio/img4.jpg';
 
 const ResultPage = () => {
   const location = useLocation();
 
-
-  const name = sessionStorage.getItem("name")
-  const email = sessionStorage.getItem("email")
-  const height = sessionStorage.getItem("height")
-  const weight = sessionStorage.getItem("weight")
-  const age = sessionStorage.getItem("age")
-  const sex = sessionStorage.getItem("sex")
-  const fitnessLevel = sessionStorage.getItem("fitnessLevel")
-  const diabetesType = sessionStorage.getItem("diabetesType")
-  const dietaryRestrictions = sessionStorage.getItem("dietaryRestrictions")
-  const cal = sessionStorage.getItem("cal")
+  // Get stored data from sessionStorage
+  const name = sessionStorage.getItem("name");
+  const email = sessionStorage.getItem("email");
+  const height = sessionStorage.getItem("height");
+  const weight = sessionStorage.getItem("weight");
+  const age = sessionStorage.getItem("age");
+  const sex = sessionStorage.getItem("sex");
+  const fitnessLevel = sessionStorage.getItem("fitnessLevel");
+  const diabetesType = sessionStorage.getItem("diabetesType");
+  const dietaryRestrictions = sessionStorage.getItem("dietaryRestrictions");
+  const cal = sessionStorage.getItem("cal");
 
   return (
     <div>
-      <h1>Result Page</h1> 
-      {name ? (
-        <div>
-          <p><strong>Name:</strong> {name}</p>
-          <p><strong>Email:</strong> {email}</p>
-          <p><strong>Height:</strong> {height}</p>
-          <p><strong>Weight:</strong> {weight}</p>
-          <p><strong>Age:</strong> {age}</p>
-          <p><strong>Sex:</strong> {sex}</p>
-          <p><strong>Fitness Level:</strong> {fitnessLevel}</p>
-          <p><strong>Diabetes Type:</strong> {diabetesType}</p>
-          <p><strong>Dietary Restrictions:</strong> {dietaryRestrictions}</p>
-        </div>
-      ) : (
-        <p>No data available</p>
-      )}
+      {/* User Info Section */}
+      
+
+      {/* Portfolio Section */}
+      <div className="spacer">
+        <Container>
+          <Row className="justify-content-center">
+            <Col md="7" className="text-center">
+              <h2 className="title">Our Recent Work</h2>
+              <h6 className="subtitle">You can rely on our amazing features list and customer service.</h6>
+            </Col>
+          </Row>
+          <Row className="m-t-40">
+            {/* First Row: 2 items */}
+            <Col md="6">
+              <Card className="card-shadow">
+                <a href="#" className="img-ho">
+                  <img className="card-img-top" src={img1} alt="Portfolio item 1" />
+                </a>
+                <CardBody>
+                  <h5 className="font-medium m-b-0">Branding for Theme Designer</h5>
+                  <p className="m-b-0 font-14">Digital Marketing</p>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col md="6">
+              <Card className="card-shadow">
+                <a href="#" className="img-ho">
+                  <img className="card-img-top" src={img2} alt="Portfolio item 2" />
+                </a>
+                <CardBody>
+                  <h5 className="font-medium m-b-0">Button Designs Free</h5>
+                  <p className="m-b-0 font-14">Search Engine</p>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+          <Row className="m-t-40">
+            {/* Second Row: 2 items */}
+            <Col md="6">
+              <Card className="card-shadow">
+                <a href="#" className="img-ho">
+                  <img className="card-img-top" src={img3} alt="Portfolio item 3" />
+                </a>
+                <CardBody>
+                  <h5 className="font-medium m-b-0">Sionage Mockup</h5>
+                  <p className="m-b-0 font-14">Wall Mockup</p>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col md="6">
+              <Card className="card-shadow">
+                <a href="#" className="img-ho">
+                  <img className="card-img-top" src={img4} alt="Portfolio item 4" />
+                </a>
+                <CardBody>
+                  <h5 className="font-medium m-b-0">Hard Cover Book Mock</h5>
+                  <p className="m-b-0 font-14">Book Covers</p>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 };
--- src/views/custom-components/sections/portfoliocomponent.jsx
+++ src/views/custom-components/sections/portfoliocomponent.jsx
@@ -6,8 +6,6 @@ import img1 from '../../../assets/images/portfolio/img1.jpg';
 import img2 from '../../../assets/images/portfolio/img2.jpg';
 import img3 from '../../../assets/images/portfolio/img3.jpg';
 import img4 from '../../../assets/images/portfolio/img4.jpg';
-import img5 from '../../../assets/images/portfolio/img5.jpg';
-import img6 from '../../../assets/images/portfolio/img6.jpg';
 
 const PortfolioComponent = () => {
     return (
@@ -17,7 +15,7 @@ const PortfolioComponent = () => {
                     <Row className="justify-content-center">
                         <Col md="7" className="text-center">
                             <h1 className="title font-bold">Portfolio</h1>
-                            <h6 className="subtitle">Here you can check Demos we created based on WrapKit. Its quite easy to Create your own dream website &amp; dashboard in No-time.</h6>
+                            <h6 className="subtitle">Here you can check Demos we created based on WrapKit. It's quite easy to create your own dream website &amp; dashboard in no time.</h6>
                         </Col>
                     </Row>
                 </Container>
@@ -26,59 +24,53 @@ const PortfolioComponent = () => {
                 <Container>
                     <Row className="justify-content-center">
                         <Col md="7" className="text-center">
-                            <h2 className="title">Our Recent work with three column</h2>
-                            <h6 className="subtitle">You can relay on our amazing features list and also our customer services will be great experience for you without doubt and in no-time</h6>
+                            <h2 className="title">Our Recent Work</h2>
+                            <h6 className="subtitle">You can rely on our amazing features list, and our customer service will be a great experience for you without a doubt and in no time.</h6>
                         </Col>
                     </Row>
                     <Row className="m-t-40">
-                        <Col md="4">
+                        {/* First Row: 2 items */}
+                        <Col md="6">
                             <Card className="card-shadow">
-                                <a href="#" className="img-ho"><img className="card-img-top" src={img1} alt="wrappixel kit" /></a>
+                                <a href="#" className="img-ho">
+                                    <img className="card-img-top" src={img1} alt="wrappixel kit" />
+                                </a>
                                 <CardBody>
                                     <h5 className="font-medium m-b-0">Branding for Theme Designer</h5>
                                     <p className="m-b-0 font-14">Digital Marketing</p>
                                 </CardBody>
                             </Card>
                         </Col>
-                        <Col md="4">
+                        <Col md="6">
                             <Card className="card-shadow">
-                                <a href="#" className="img-ho"><img className="card-img-top" src={img2} alt="wrappixel kit" /></a>
+                                <a href="#" className="img-ho">
+                                    <img className="card-img-top" src={img2} alt="wrappixel kit" />
+                                </a>
                                 <CardBody>
                                     <h5 className="font-medium m-b-0">Button Designs Free</h5>
                                     <p className="m-b-0 font-14">Search Engine</p>
                                 </CardBody>
                             </Card>
                         </Col>
-                        <Col md="4">
+                    </Row>
+                    <Row className="m-t-40">
+                        {/* Second Row: 2 items */}
+                        <Col md="6">
                             <Card className="card-shadow">
-                                <a href="#" className="img-ho"><img className="card-img-top" src={img3} alt="wrappixel kit" /></a>
+                                <a href="#" className="img-ho">
+                                    <img className="card-img-top" src={img3} alt="wrappixel kit" />
+                                </a>
                                 <CardBody>
-                                    <h5 className="font-medium m-b-0">Branding & Co Agency</h5>
-                                    <p className="m-b-0 font-14">Admin templates</p>
+                                    <h5 className="font-medium m-b-0">Sionage Mockup</h5>
+                                    <p className="m-b-0 font-14">Wall Mockup</p>
                                 </CardBody>
                             </Card>
                         </Col>
-                        <Col md="4">
+                        <Col md="6">
                             <Card className="card-shadow">
-                                <a href="#" className="img-ho"><img className="card-img-top" src={img4} alt="wrappixel kit" /></a>
-                                <CardBody>
-                                    <h5 className="font-medium m-b-0">Zukandre Phoniex</h5>
-                                    <p className="m-b-0 font-14">Branding</p>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col md="4">
-                            <Card className="card-shadow">
-                                <a href="#" className="img-ho"><img className="card-img-top" src={img5} alt="wrappixel kit" /></a>
-                                <CardBody>
-                                    <h5 className="font-medium m-b-0">Sionage Mokcup</h5>
-                                    <p className="m-b-0 font-14">Wll Mockup</p>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col md="4">
-                            <Card className="card-shadow">
-                                <a href="#" className="img-ho"><img className="card-img-top" src={img6} alt="wrappixel kit" /></a>
+                                <a href="#" className="img-ho">
+                                    <img className="card-img-top" src={img4} alt="wrappixel kit" />
+                                </a>
                                 <CardBody>
                                     <h5 className="font-medium m-b-0">Hard Cover Book Mock</h5>
                                     <p className="m-b-0 font-14">Book Covers</p>
