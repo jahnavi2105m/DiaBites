267ce5f4e9efe1dd7a8c2e871c1a79a4795c7182
Jahnavi Malhotra <jahnavim2105@gmail.com>
2024-09-15 01:43:53 -0400

    deleted footer

--- src/views/components/sections/aboutus.jsx
+++ src/views/components/sections/aboutus.jsx
@@ -16,7 +16,7 @@ const AboutUs = () => {
                         
                             <motion.h1 initial={{ opacity: 0, y:50 }} whileInView={{ opacity: 1, y: 0, transition: {delay: 0.2 , duration: 0.5} }} viewport={{once:false, amount: 0.5}} className="title font-bold">About Us</motion.h1>
                             
-                            <motion.p initial={{ opacity: 0, y:50 }} whileInView={{ opacity: 1, y: 0, transition: {delay: 0.2 , duration: 0.5} }} viewport={{once:false, amount: 0.5}} className="m-t-90 m-b-30">At Diabite, we are dedicated to transforming how people with diabetes manage their health. Our mission is to promote a more holistic approach to diabetes care, focusing on the power of personalized nutrition rather than relying solely on medications and needles. </motion.p>
+                            <motion.p initial={{ opacity: 0, y:50 }} whileInView={{ opacity: 1, y: 0, transition: {delay: 0.2 , duration: 0.5} }} viewport={{once:false, amount: 0.5}} className="m-t-90 m-b-30">At Diabite, we are dedicated to transforming how people with diabetes manage their health. Our mission is to promote a healthier approach to diabetes care, focusing on the power of personalized nutrition rather than relying solely on medications and needles. </motion.p>
                                
                            
                             <motion.p initial={{ opacity: 0, y:50 }} whileInView={{ opacity: 1, y: 0, transition: {delay: 0.2 , duration: 0.5} }} viewport={{once:false, amount: 0.5}} className="m-t-70 m-b-30"> By understanding each individual’s unique dietary needs, our platform generates customized meal plans designed to support balanced blood sugar levels and overall well-being.
