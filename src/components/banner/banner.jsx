d1020cec80c05c3ac14209a885e89e9ed2c62218
Jahnavi Malhotra <jahnavim2105@gmail.com>
2024-09-15 02:26:27 -0400

    update name

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -9,7 +9,7 @@ const HeaderBanner = () => {
             <Container>
                 <Row className="justify-content-center">
                     <Col lg="8" md="6" className="align-self-center text-center">
-                        <motion.h1 initial={{ opacity: 0, y:50 }} whileInView={{ opacity: 1, y: 0, transition: {delay: 0.2 , duration: 0.5} }} viewport={{once:false, amount: 0.5}} className="title">DiaBite</motion.h1>
+                        <motion.h1 initial={{ opacity: 0, y:50 }} whileInView={{ opacity: 1, y: 0, transition: {delay: 0.2 , duration: 0.5} }} viewport={{once:false, amount: 0.5}} className="title">DiaBites</motion.h1>
                         <motion.h4 initial={{ opacity: 0, y:50 }} whileInView={{ opacity: 1, y: 0, transition: {delay: 0.2 , duration: 0.5} }} viewport={{once:false, amount: 0.5}} className="subtitle font-light">Ditch the Needle, Grab the Fork<br /> </motion.h4>
                         <Link initial={{ opacity: 0, y:50 }} whileInView={{ opacity: 1, y: 0, transition: {delay: 0.2 , duration: 0.5} }} viewport={{once:false, amount: 0.5}} to="/forms" className="btn btn-md m-t-10 btn-info-gradiant font-14">Take our Quiz</Link>
                     </Col>
